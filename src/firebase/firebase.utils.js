6a798a6879f9945b46d85076ea8b5cd8bb98ebba
kunal prakash <kunalprakash1309@gmail.com>
2020-10-18 12:34:43 +0530

    implements firebase complete signIn and signUp using firebase auth and firestore to store data

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,6 +19,35 @@ const config = {
     measurementId: "G-28S0GNYGYS"
 }
 
+// for database
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    console.log(userRef)
+    
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch(err) {
+            console.log("error creating user", err)
+        }
+    }
+
+    return userRef
+}
+
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
