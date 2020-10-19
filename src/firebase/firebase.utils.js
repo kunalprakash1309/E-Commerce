50ec6521583609f5817df3da52505846f0a1449a
kunal prakash <kunalprakash1309@gmail.com>
2020-10-19 22:22:51 +0530

    comment

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -53,6 +53,7 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+// for setting google auth
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
